13eafa906df4f0d82a467c3b345f5bb65d513e76
korotkovaleksandr <korotkovaleksandrg@gmail.com>
2024-03-18 14:04:20 +0300

    Initial Commit

---
+++ Maket_hh_App/Maket_hh_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maket_hh_AppApp.swift
+//  Maket_hh_App
+//
+//  Created by Александр Коротков on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Maket_hh_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
